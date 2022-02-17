0a64bcad718a4a4cf8b8a27ae42c3fc33ab64c3f
SUHAN447 <suhanucuretci7@gmail.com>
2022-02-17 20:51:19 +0300

    firebase

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include "FirebaseESP8266.h"
 
 #define SSID "VEnus Z20"
 #define PWD "azsxdcfv447"
 #define DHTPIN 14
 #define DHTTYPE DHT21
+
+#define FIREBASE_HOST "nodemcu-9b1c1-default-rtdb.firebaseio.com/" // http:// veya https:// olmadan yazın
+#define FIREBASE_AUTH "WNcSJaL959DpNdx15nkqa0DtPknsVyPDDH4kGK8u"
+
+FirebaseData veritabanim;
 
 ESP8266WebServer server(80);
 
@@ -23,6 +29,7 @@ DHT dht(DHTPIN, DHTTYPE);
 void dhtPrint();
 void getDHT();
 void cliPrint();
+void handleFirebase();
 
 void setup()
 {
@@ -35,10 +42,18 @@ void setup()
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
 
-    if (!WiFi.config(local_IP, gateway, subnet))
-    {
-        Serial.println("Wifi Failed to configure");
-    }
+    /*
+        if (!WiFi.config(local_IP, gateway, subnet))
+        {
+            Serial.println("Wifi Failed to configure");
+        }
+        */
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+    // 4. Ağ bağlantısı kesilirse tekrar bağlanmasına izin veriyoruz
+    Firebase.reconnectWiFi(true);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
 
     server.on("/test", HTTP_GET, cliPrint);
     server.begin();
@@ -49,6 +64,7 @@ void setup()
 void loop()
 {
     server.handleClient();
+    handleFirebase();
 }
 
 void dhtPrint()
@@ -99,4 +115,31 @@ void get()
 void cliPrint()
 {
     server.send(200, "text / plain", "test");
+}
+
+void handleFirebase()
+{
+    if (Firebase.getString(veritabanim, "/led")) // Alınacak veri tipine göre getInt, getBool, getFloat, getDouble, getString olarak kullanılabilir.
+    {
+        // bağlantı başarılı ve veri geliyor ise
+        // Serial.print("String tipinde veri alımı başarılı, veri = ");
+        // Serial.println(veritabanim.stringData());
+        if (veritabanim.stringData() == "1")
+        {
+            digitalWrite(LED_BUILTIN, LOW);
+            Firebase.setBoolAsync(veritabanim, "test", true);
+        }
+        else
+        {
+            digitalWrite(LED_BUILTIN, HIGH);
+            Firebase.setBoolAsync(veritabanim, "test", false);
+        }
+    }
+    else
+    {
+        // hata varsa hata mesajı ve nedeni yazdırılıyor
+
+        Serial.print("Str verisi çekilemedi, ");
+        Serial.println(veritabanim.errorReason());
+    }
 }
